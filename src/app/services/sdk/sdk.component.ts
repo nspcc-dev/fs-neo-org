bac3b120655e318b230261dcc50c51fc5e7c61f6
anatoly@nspcc.ru <anatoly@nspcc.ru>
2019-11-04 15:04:43 +0300

    update

--- src/app/services/sdk/sdk.component.ts
+++ src/app/services/sdk/sdk.component.ts
@@ -7,11 +7,9 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./sdk.component.css']
 })
 export class SdkComponent implements OnInit {
-  public safeURL: any;
   public safeURL_v2: any;
 
   constructor(private _sanitizer: DomSanitizer) {
-    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/2o44JvXyy0o');
     this.safeURL_v2 = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/4f_q5mVsiXY');
    }
 
