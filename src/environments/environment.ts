d6c9037a45f78c9c1c36a5cab9d72ccc80a6f94c
anatoly@nspcc.ru <anatoly@nspcc.ru>
2020-05-21 11:24:27 +0300

    fix node_rpc issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   
   neofs_api: "http://localhost:4200/api", 
-  neo_rpc: "http://localhost:4200/neo_rpc",
+  neo_rpc: "http://localhost:4200/neo_rpc/",
   neofs_send_url: "https://send.fs.neo.org",
   gas_asset: "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7",
   neofs_sc: "67560720693416608bda4e4d7f9b665c5690dbfc"
