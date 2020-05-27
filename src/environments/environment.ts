c8c68ce90df918cd8ecff6f677dc1990cab2eaca
anatoly@nspcc.ru <anatoly@nspcc.ru>
2020-05-27 20:53:30 +0300

    update multi lenguage support

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   gas_asset: "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7",
   neofs_sc: "67560720693416608bda4e4d7f9b665c5690dbfc",
 
-  locales: ['en', 'ru'],
+  locales: ['en', 'ru', 'zh'],
   defaultLocale: 'en',
 };
 
