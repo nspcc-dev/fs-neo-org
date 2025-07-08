d96c92330b0bfaa71b6e73e5d79fae6aef780c8c
Mikhail Petrov <mike@nspcc.ru>
2025-07-08 10:26:25 +0300

    web: Add replica count for storage price calculator
    closes #58
    
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- themes/neofs-theme/static/js/main.js
+++ themes/neofs-theme/static/js/main.js
@@ -4,9 +4,13 @@ async function getNetworkInfo() {
 	return text;
 }
 
-const onUpdateCalculatorValue = (value, storagePrice, epochDuration, calculatorValue, calculatorPriceValue) => {
+const onUpdateCalculatorValue = (value, replicas, storagePrice, epochDuration, calculatorValue, calculatorPriceValue, calculatorReplicasValue) => {
 	let capacity = Math.pow(2, value);
-	calculatorPriceValue.textContent = ((capacity / 1024) * storagePrice * (30 * 24 * 60 * 60) / epochDuration  / 1e12).toFixed(8);
+
+	const pricePerOne = (capacity / 1024) * storagePrice * (30 * 24 * 60 * 60) / epochDuration  / 1e12;
+	const totalPrice = (pricePerOne * replicas).toFixed(8);
+	calculatorPriceValue.textContent = totalPrice;
+	calculatorReplicasValue.textContent = replicas;
 	if (capacity < 1024) {
 		measurement = 'MB';
 	} else if (capacity < 1048576) {
@@ -39,12 +43,14 @@ $(document).ready(function () {
 	}).finally(() => {
 		const calculatorValue = document.querySelector("#calculator_value");
 		const calculatorPriceValue = document.querySelector("#calculator_price_value");
+		const calculatorReplicasValue = document.querySelector("#calculator_replicas_value");
 		const calculator = document.querySelector("#range");
-		if (calculator) {
-			calculator.value = 10;
-		}
+		const replicas = document.querySelector("#range_replicas");
+		if (calculator) calculator.value = 10;
+		if (replicas) replicas.value = 3;
 
-		onUpdateCalculatorValue(10, storagePrice, epochDuration, calculatorValue, calculatorPriceValue)
-		$("#range").on('input', (e) => onUpdateCalculatorValue(e.target.value, storagePrice, epochDuration, calculatorValue, calculatorPriceValue));
+		onUpdateCalculatorValue(10, 3, storagePrice, epochDuration, calculatorValue, calculatorPriceValue, calculatorReplicasValue)
+		$("#range").on('input', (e) => onUpdateCalculatorValue(e.target.value, replicas.value, storagePrice, epochDuration, calculatorValue, calculatorPriceValue, calculatorReplicasValue));
+		$("#range_replicas").on('input', (e) => onUpdateCalculatorValue(calculator.value, e.target.value, storagePrice, epochDuration, calculatorValue, calculatorPriceValue, calculatorReplicasValue));
 	});
 });
